ee3040b60d5ee1c84ce129ecc4d1ac5ff25919f5
optimus9021 <ismaianugrah@gmail.com>
2025-02-06 15:03:02 +0700

    Feature: Slashing Feature

--- src/app/pageClient/home/page.tsx
+++ src/app/pageClient/home/page.tsx
@@ -5,6 +5,7 @@ import OurLoading from "@/components/OurLoading";
 export default function HomePage() {
     return (
         <div className="flex items-center justify-center h-screen">
+            Lagi loading nih...
             <OurLoading />
         </div>
     );
