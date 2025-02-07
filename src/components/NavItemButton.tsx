247fa084297432dfb09b9f85e1097eaf9b11d800
optimus9021 <ismaianugrah@gmail.com>
2025-02-07 11:31:39 +0700

    Feature: Fix Mode Toggle

--- src/components/NavItemButton.tsx
+++ src/components/NavItemButton.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 
 interface NavItemButtonProps {
     isActive: boolean;
@@ -12,6 +12,25 @@ interface NavItemButtonProps {
     isChild?: boolean;
 }
 
+// export default function NavItemButton({ isActive, isLoading, route, isChild }: NavItemButtonProps) {
+//     return (
+//         <Link href={route.link} passHref>
+//             <Button
+//                 variant={isActive && !isChild ? "default" : "ghost"}
+//                 className={`w-full justify-start ${
+//                     isActive ? "font-bold dark:hover:bg-gray-500 dark:hover:text-white" : "dark:text-white dark:hover:bg-gray-500 dark:hover:text-white"
+//                 }`}
+//             >
+//                 {isLoading ? "🔄" : route.icon && <route.icon className="mr-3 size-5" />}
+//                 {route.title}
+//             </Button>
+//         </Link>
+//     );
+// }
+
+import { Loader } from "lucide-react";
+import { Button } from "./ui/button";
+
 export default function NavItemButton({ isActive, isLoading, route, isChild }: NavItemButtonProps) {
     return (
         <Link href={route.link} passHref>
@@ -21,7 +40,7 @@ export default function NavItemButton({ isActive, isLoading, route, isChild }: N
                     isActive ? "font-bold dark:hover:bg-gray-500 dark:hover:text-white" : "dark:text-white dark:hover:bg-gray-500 dark:hover:text-white"
                 }`}
             >
-                {isLoading ? "🔄" : route.icon && <route.icon className="mr-3 size-5" />}
+                {isLoading ? <Loader className="animate-spin h-5 w-5 mr-3" /> : route.icon && <route.icon className="mr-3 size-5" />}
                 {route.title}
             </Button>
         </Link>
