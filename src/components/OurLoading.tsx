548489302a3e53e90c76c15b40389381fb369190
optimus9021 <ismaianugrah@gmail.com>
2025-02-11 09:24:28 +0700

    Feature: Add Loading Screen

--- src/components/OurLoading.tsx
+++ src/components/OurLoading.tsx
@@ -4,6 +4,7 @@ import { useTheme } from 'next-themes';
 const OurLoading = () => {
 	const { theme } = useTheme();
 	return (
+		<div className="flex items-center justify-center h-screen">
 		<div className="relative w-24 h-24 animate-pulse">
 			<Image
 				src={theme === "dark" ? "/logo_icon.svg" : "/logo_icon_d.svg"}
@@ -12,6 +13,7 @@ const OurLoading = () => {
 				height={96}
 				className="transition-transform duration-500 ease-in-out transform hover:scale-110"
 			/>
+		</div>
 		</div>
 	);
 };
