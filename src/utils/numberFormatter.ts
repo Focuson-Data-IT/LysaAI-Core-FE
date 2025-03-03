fab4ae150755fbecd6a1553201b876c74bf9d0da
optimus9021 <ismaianugrah@gmail.com>
2025-03-03 16:27:16 +0700

    revisi

--- src/utils/numberFormatter.ts
+++ src/utils/numberFormatter.ts
@@ -12,7 +12,10 @@ export const numberFormatter = (value) => {
     if (value >= 1_000_000) return (value / 1_000_000).toFixed(0) + "M";
     if (value >= 1_000) return (value / 1_000).toFixed(0) + "K";
     
-    return value.toString(); // Pastikan angka dikembalikan dalam bentuk string
+    return value.toLocaleString("id-ID", {
+		minimumFractionDigits: 1,
+		maximumFractionDigits: 1
+	});
 };
 
 
